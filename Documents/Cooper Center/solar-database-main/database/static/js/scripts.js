ad0262d418eccf0335f9bc056f6fc6382526accc
ahn5nc <ahn5nc@virginia.edu>
2025-09-03 10:04:27 -0400

    continuing ham menu, fixed navbar issue with scrollbar

--- Documents/Cooper Center/solar-database-main/database/static/js/scripts.js
+++ Documents/Cooper Center/solar-database-main/database/static/js/scripts.js
@@ -3,4 +3,13 @@ const menu = document.getElementById('menu-items');
 
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active'); // animate hamburger
+  menu.classList.toggle('active');
+});
+
+
+window.addEventListener('resize', () => {
+    if (window.innerWidth > 993) {
+        hamburger.classList.remove('active');
+        menu.classList.remove('active');
+    }
 });
